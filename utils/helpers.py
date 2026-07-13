7bf86746520f3f3625abda729ecbd93f1e70b40a
parth5012 <parthchawla5012@GMAIL.COM>
2026-07-13 11:44:43 +0530

    Fixed prompt  for title generation

--- utils/helpers.py
+++ utils/helpers.py
@@ -3,4 +3,4 @@ from langsmith import traceable
 
 @traceable(name='Title Generation')
 def get_title(conversation:str):
-    return get_llm().invoke("Return the Title for this conversation below : \n Conversation \n" + conversation)
+    return get_llm().invoke("Return the Title for this conversation below , do not generate any other word except for the title no preamble : \n Conversation \n" + conversation)
